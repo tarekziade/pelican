3e12752ebc17a96d62fae3a9125e51f233f93cd3
Tarek Ziade <tarek@ziade.org>
2012-03-15 15:55:39 +0100

    allow using TAG_FEED_RSS without having to use TAG_FEED

--- pelican/generators.py
+++ pelican/generators.py
@@ -160,15 +160,17 @@ class ArticlesGenerator(Generator):
                         self.settings['CATEGORY_FEED_RSS'] % cat,
                         feed_type='rss')
 
-        if 'TAG_FEED' in self.settings:
+        if 'TAG_FEED' in self.settings or 'TAG_FEED_RSS' in self.settings:
             for tag, arts in self.tags.items():
                 arts.sort(key=attrgetter('date'), reverse=True)
-                writer.write_feed(arts, self.context,
-                        self.settings['TAG_FEED'] % tag)
+
+                if 'TAG_FEED' in self.settings:
+                    writer.write_feed(arts, self.context,
+                            self.settings['TAG_FEED'] % tag)
 
                 if 'TAG_FEED_RSS' in self.settings:
                     writer.write_feed(arts, self.context,
-                            self.settings['TAG_FEED_RSS'] % tag, feed_type='rss')
+                          self.settings['TAG_FEED_RSS'] % tag, feed_type='rss')
 
         translations_feeds = defaultdict(list)
         for article in chain(self.articles, self.translations):
